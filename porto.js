f92c68023c5fafbb9b09a43c75cc1466146d893f
user <user@LENOVO-PC>
2025-09-25 20:41:39 +0700

    jh

---
+++ porto.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.getElementById("helloBtn");
+  btn.addEventListener("click", function() {
+    alert("Halo, selamat datang di portofolio saya!");
+  });
+});
